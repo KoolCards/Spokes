f89920627b7bb9cc5be576474d9c10dc1bb4e69a
Akum Kang <kangakum@lawn-128-61-36-127.lawn.gatech.edu>
2018-09-29 17:48:52 -0400

    update

---
+++ app/src/main/java/com/example/ksatya/spokes/friends2.java
@@ -0,0 +1,13 @@
+package com.example.ksatya.spokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class friends2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends2);
+    }
+}
